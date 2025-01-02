1b29d4602bd506fc783f963eb49750ff374867ee
mmikusx <73282940+mmikusx@users.noreply.github.com>
2025-01-02 15:55:44 +0100

    Zadanie 2 (4.5 i 5.0)

--- TaskList/TaskList/ContentView.swift
+++ TaskList/TaskList/ContentView.swift
@@ -11,24 +11,31 @@ struct Task: Identifiable {
     let id = UUID()
     let name: String
     let imageName: String
+    var status: Bool
 }
 
 struct ContentView: View {
     @State private var tasks = [
-        Task(name: "Zrobić zakupy", imageName: "cart"),
-        Task(name: "Wyprowadzić psa", imageName: "dog"),
-        Task(name: "Skończyć projekt", imageName: "note"),
-        Task(name: "Zadzwonić gdzieś", imageName: "phone"),
-        Task(name: "Test to delete", imageName: "phone")
+        Task(name: "Zrobić zakupy", imageName: "cart", status: false),
+        Task(name: "Wyprowadzić psa", imageName: "dog", status: false),
+        Task(name: "Skończyć projekt", imageName: "note", status: false),
+        Task(name: "Zadzwonić gdzieś", imageName: "phone", status: false),
+        Task(name: "Test usuwania", imageName: "phone", status: false)
     ]
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(tasks) { task in
+                ForEach($tasks) { $task in
                     HStack {
                         Image(systemName: task.imageName)
                         Text(task.name)
+                        Spacer()
+                        Button(action: {
+                            task.status.toggle()
+                        }) {
+                            Image(systemName: task.status ? "checkmark.circle.fill" : "circle")
+                        }
                     }
                 }
                         .onDelete(perform: deleteTask)
